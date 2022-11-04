f94add2df2c917edf14df897e477bd2e05d5b22a
archdemon7 <vakylskij@gmail.com>
2022-11-04 23:13:47 +0300

    feat: add js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,45 @@
 const audio = new Audio();
 const trackCards = document.getElementsByClassName('track');
 const pauseBtn = document.querySelector('.player__icon_pause');
+const player = document.querySelector('.player');
+const stopBtn = document.querySelector('.player__icon_stop');
 
-const playMusic = url => {
-    audio.src = url;
+const playMusic = event => {
+    const trackActive = event.currentTarget;
+
+    audio.src = trackActive.dataset.track;
+    player.classList.add('player_active')
     audio.play();
+    pauseBtn.classList.remove('player__icon_play');
+
+    for (let i = 0; i < trackCards.length; i++) {
+        trackCards[i].classList.remove('track_active');
+    }
+    trackActive.classList.add('track_active');
 }
 
 for (let i = 0; i < trackCards.length; i++) {
-    trackCards[i].addEventListener('click', playMusic)
-} 
+    trackCards[i].addEventListener(('click'), playMusic);
+}
 
 pauseBtn.addEventListener('click', () => {
-    playMusic('assets/audio/09Two Feet - Grey.mp3')
+    if (audio.paused) {
+        audio.play();
+        pauseBtn.classList.remove('player__icon_play');
+        pauseBtn.classList.add('player__icon_pause');
+    } else {
+        audio.pause();
+        pauseBtn.classList.remove('player__icon_pause');
+        pauseBtn.classList.add('player__icon_play');
+    }
+});
+
+stopBtn.addEventListener('click', () => {
+    if (audio.played) {
+        audio.pause();
+        audio.currentTime = 0;
+        pauseBtn.classList.remove('player__icon_pause');
+        pauseBtn.classList.add('player__icon_play');
+        player.classList.remove('player_active');
+    }
 })
